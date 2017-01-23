8dfa21a81f208cdc81af6e255b1a3f222b228999
markusvhagen <markusvhagen@gmail.com>
2017-01-23 18:50:37 +0100

    Added function sov()

--- jorre_slider/indexInter.js
+++ jorre_slider/indexInter.js
@@ -1,12 +1,23 @@
+function sov(ms) {
+    var tid = new Date().getTime();
+    while (tid + ms >= new Date().getTime()) {}
+}
+
 window.onload = function () {
   var ting = document.getElementById("bilder");
   var flytt = 0;
-  var eine = setInterval(ein ,16);
-  function ein(){
-    flytt += 5;
-    ting.style.left = "-" + flytt + "px";
-    if (flytt == 1600) {
-        flytt = 0;
-    }
-  }
-}
+
+  setInterval(function bevegelse() {
+      flytt += 5;
+      ting.style.left = "-" + flytt + "px";
+
+      if (flytt == 1600) {
+          flytt = 0;
+      }
+
+      if (flytt == 400) {
+          sov(500);
+      }
+  }, 16);
+
+ }
